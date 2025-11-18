861354ea0ea99bc4be8d5dbedbcdf36a91baf46c
Mustafa Fatih Can <canmustafafatih@gmail.com>
2025-11-18 15:47:07 +0300

    reset project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
-export default defineConfig({
-  site: "https://mustafafatihcan.netlify.app/",
-  integrations: [preact()]
-});
+export default defineConfig({});
