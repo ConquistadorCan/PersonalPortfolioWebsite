dcde74bcb727c6c64c248a18562aedb41c4dba02
Mustafa Fatih Can <canmustafafatih@gmail.com>
2025-11-20 14:52:01 +0300

    base layour, header, footer

---
+++ src/config/consts.ts
@@ -0,0 +1,8 @@
+export const SITE_OWNER_NAME = "Mustafa Fatih"
+export const SITE_OWNER_SURNAME = "Can"
+export const SITE_OWNER_JOB = "Software Engineer"
+export const SITE_TITLE = `${SITE_OWNER_NAME} ${SITE_OWNER_SURNAME} | ${SITE_OWNER_JOB}`
+export const SITE_OWNER_EMAIL = "canmustafafatih@gmail.com"
+export const SITE_OWNER_GITHUB = "https://github.com/ConquistadorCan"
+export const SITE_OWNER_LINKEDIN = "https://www.linkedin.com/in/mustafafatihcan/"
+export const CURRENT_YEAR = new Date().getFullYear()
